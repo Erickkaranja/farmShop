51045d523764f7bd1c99bd224350f760b0d9192a
Erickkaranja <karanja99erick@gmail.com>
2024-09-28 22:52:37 +0300

    finishing on footer

--- website/script/home.js
+++ website/script/home.js
@@ -15,26 +15,39 @@ function toggleMenu() {
         menu.style.display = "none";
       }
     };
-/**
-  farmer json data format
-  {
-   ['farmName': 'farm-name', 'products': '[roduct-1, product-2]', 'description': 'this farm 1',
-     'location': 'Kajiado, Kenya', 'farm-images': '[image-1, image-2, image-3]', 'contact': '0123456'
-]
-}
-**/
+
 function createFarmerDetails(data) {
    let currentIndex = 0;
    const farmerDetailsDiv = document.querySelector('.category');
    const farmerSection = document.createElement('div');
-   farmSection.classList.add('exa');
-   farmSection.style.backgroundImage = data.images[currentIndex];
-   const farmName = document.createElement('h2');
-   farmName.setAttribute('id', 'farmName');
-   farmName.textContent = data.farmName;
-   farmName.style.position = 'absolute';
+   farmerSection.classList.add('exa');
+   farmerSection.style.backgroundImage = data.images[currentIndex];
 
-   farmSection.appendChild('farmName');
+   const farmDescription = document.createElement('div');
+   farmDescription.classList.add('description');
+   const farmName = document.createElement('p');
+   farmName.textContent = data.farmName;
+   farmName.style.testSize = '20px';
+   farmName.style.color = '#0d0d0d';
+   farmName.style.fontWeight = 'bold';
+   const farmLocation = document.createElement('p');
+   farmLocation.textContent = data.farmLocation;
+ 
+   let content = "";
+   data.produce.forEach(product => {
+     content += product + ', ';
+   })
+   const produce = document.createElement('p');
+     produce.style.textSize = '10px';
+     produce.style.color = '#808080';
+     produce.textContent = content;
+
+   farmDescription.appendChild(farmName);
+   farmDescription.appendChild(produce);
+   farmDescription.appendChild(farmLocation);
+   farmDescription.style.position = 'relative';
+   farmerSection.appendChild(farmDescription);
+
    // creating next and previous button
    const prevButton = document.createElement('button');
      prevButton.classList.add('prev');
@@ -49,9 +62,9 @@ function createFarmerDetails(data) {
      nextButton.onclick = nextImage;
 
    farmerSection.appendChild(prevButton);
-   farmSection.appendChild(nextButton);
+   farmerSection.appendChild(nextButton);
 
-   farmerDetailsDiv.appendChild(farmSection);
+   farmerDetailsDiv.appendChild(farmerSection);
 }
 function prevImage() {
      currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
@@ -62,3 +75,13 @@ function nextImage() {
      currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
      imageBox.style.backgroundImage = images[currentIndex];
   }
+const data = {'farmName': 'Wingo beef farm', 'produce': ['roduct-1', 'product-2'], 'description': 'this farm 1',
+     'farmLocation': 'Kajiado, Kenya', 'images': '[image-1, image-2, image-3]', 'contact': '0123456'}
+
+createFarmerDetails(data);
+
+const newData = {'farmName': 'farm-name', 'produce': ['roduct-1', 'product-2'], 'description': 'this farm 1',
+     'farmLocation': 'Kajiado, Kenya', 'images': '[image-1, image-2, image-3]', 'contact': '0123456'}
+for(let i=0; i < 14; i++) {
+ createFarmerDetails(newData);
+}
